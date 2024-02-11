fd5a570de39ed0edba1ece75732dec8e919e8dea
HamzaDmitriyevich <krou1937@gmail.com>
2024-02-11 22:35:53 +0300

    Добавил конструкцию ifelse и сайдбар вылазит

--- project.js
+++ project.js
@@ -1,14 +1,20 @@
 window.addEventListener(
     'load',
     () => {
+
         var sidebar = document.getElementById('sidebar');
 var menubar1 = document.getElementById('menubar1');
-
 menubar1.addEventListener(
+   
     'click',
-    
+
     function(){
     sidebar.classList.toggle('active');
+    if(sidebar.style.left=== '-200px'){
+        sidebar.style.left = '0px';
+    }else{
+        sidebar.style.left = '-200px';
+    }
     });
     }
 );
