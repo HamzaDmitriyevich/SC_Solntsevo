14fd7cb000afd78205bbae4464b9566251364fbe
HamzaDmitriyevich <krou1937@gmail.com>
2024-02-11 21:30:09 +0300

    Дописал Скрипт к боковому меню

---
+++ project.js
@@ -0,0 +1,11 @@
+var sidebar = document.getElementById('sidebar');
+var menubarFc = document.getElementById('menubar');
+
+menubarFc.addEventListener(
+    'click',
+    
+    function(){
+    sidebar.classList.toggle('active');
+    });
+
+
